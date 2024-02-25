c3428a452bf67387417f9ebfa2d6b14633d6b1d4
Berko-Adepa-Agnes <156245300+Berko-Adepa-Agnes@users.noreply.github.com>
2024-02-26 01:07:45 +0700

    next updated select -emails.sh

--- negpod_16-q1/select-emails.sh
+++ negpod_16-q1/select-emails.sh
@@ -3,3 +3,4 @@
 # Select emails of all students from students-list_1023.txt
 cut -d',' -f1 students-list_1023.txt > student-emails.txt
 echo "Student emails selected and saved to student-emails.txt"
+
